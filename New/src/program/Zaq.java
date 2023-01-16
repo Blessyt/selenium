ea017625c163f2cb08a0e2ae1b1d7bbd40c00c65
Blessy <abc@gmail.com>
2023-01-16 15:57:26 +0530

    Helooo

--- New/src/program/Zaq.java
+++ New/src/program/Zaq.java
@@ -2,4 +2,10 @@ package program;
 
 public class Zaq {
 
+public static void main(String[] args) {
+
+	// TODO Auto-generated method stub
+	System.out.println("Hello World!!!!!");
+	System.out.println("Hiii");
+}
 }
